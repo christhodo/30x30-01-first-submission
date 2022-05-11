5e6ddd50ecdfa7ed84f139a6524f9ee164f245e5
christhodo <102981895+christhodo@users.noreply.github.com>
2022-05-11 13:07:32 -0700

    first submission for 30x30

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CoursesComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../common/models/course';
+import { CoursesService } from '../common/services/courses.service';
+
+const emptyCourse: Course = {
+  id: null,
+  title: '',
+  description: '',
+  percentComplete: 0,
+  favorite: false,
+};
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+})
+export class CoursesComponent implements OnInit {
+  courses = [];
+  selectedCourse = emptyCourse;
+  originalTitle = '';
+
+  constructor(private coursesService: CoursesService) {}
+
+  ngOnInit(): void {
+    this.fetchCourses();
+  }
+
+  selectCourse(course) {
+    this.selectedCourse = { ...course };
+    this.originalTitle = course.title;
+  }
+
+  fetchCourses() {
+    this.coursesService
+      .all()
+      .subscribe((result: any) => (this.courses = result));
+  }
+
+  saveCourse(course) {
+    if (course.id) {
+      this.updateCourse(course);
+    } else {
+      this.createCourse(course);
+    }
+  }
+
+  createCourse(course) {
+    this.coursesService
+      .create(course)
+      .subscribe((result) => this.fetchCourses());
+  }
+
+  updateCourse(course) {
+    this.coursesService
+      .update(course)
+      .subscribe((result) => this.fetchCourses());
+  }
+
+  deleteCourse(courseId) {
+    console.log('Delete Course', courseId);
+  }
+
+  reset() {
+    this.selectCourse({ ...emptyCourse });
+  }
+}
